6b5a871062c0e5a21ea56cb4e08ad1c04efe15e6
yuan hehe <helpzp2008@126.com>
2017-06-19 15:47:19 +0800

    增加异步验证

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
- SPA_RESOLVE_INIT = function(transition) {
- 	console.log(document.getElementById("tpl_home").innerHTML)
-	document.getElementById("content").innerHTML = document.getElementById("tpl_home").innerHTML;
-	console.log(document.getElementById("tpl_home").innerHTML)
-}
+ SPA_RESOLVE_INIT = function (transition) {
+ 	console.log(document.getElementById("index") || '无法显示')
+ 	document.getElementById("content").innerHTML = document.getElementById("tpl_home").innerHTML;
+ 	console.log(document.getElementById("index").innerHTML)
+ }
