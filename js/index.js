e44a6b86d030f9571a9cb109794893a80caffe59
yuan hehe <helpzp2008@126.com>
2017-06-19 15:31:06 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,5 @@
+ SPA_RESOLVE_INIT = function(transition) {
+ 	console.log(document.getElementById("tpl_home").innerHTML)
+	document.getElementById("content").innerHTML = document.getElementById("tpl_home").innerHTML;
+	console.log(document.getElementById("tpl_home").innerHTML)
+}
